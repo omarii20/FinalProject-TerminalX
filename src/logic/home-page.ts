c904a5dcc65fcf98afecd7567701705aa0cc9c60
Kamil Abu <kamilabuelhija@gmail.com>
2024-01-15 13:09:28 +0200

    fix components

---
+++ src/logic/home-page.ts
@@ -0,0 +1,44 @@
+import { Page } from "playwright";
+import { BasePage } from "../infra/base-page";
+import { Header } from "./components/header";
+import { LoginPopUp } from "./components/pop-up-login";
+
+export class HomePage extends BasePage {
+    
+  private header: Header;
+  private popuplogin: LoginPopUp;
+
+  constructor(page: Page) {
+    super(page);
+    this.header = new Header(page);
+    this.popuplogin = new LoginPopUp(page);
+  }
+
+  async clicOnConnectionHeaderBtn() {
+    await this.header.clickOnConnectionBtn();
+  }
+
+  async clicOnSearchHeaderBtn() {
+    await this.header.clickOnSearchIcon();
+  }
+
+  async fillSearchInputHeader(searchItem: string) {
+    await this.header.fillInSearchItem(searchItem);
+  }
+
+  async fillPopUpLoginEmail(input: string) {
+    await this.popuplogin.fillEmail(input);
+  }
+
+  async fillPopUpLoginPassword(input: string) {
+    await this.popuplogin.fillPassword(input);
+  }
+
+  async clickPopUpLoginBtn() {
+    await this.popuplogin.clickOnLoginButton();
+  }
+
+  async fullLoginProcess(emailInput: string, passwordInput: string) {
+    await this.popuplogin.fullLoginProcess(emailInput, passwordInput);
+  }
+}
--- src/tests/search.spec.ts
+++ src/tests/search.spec.ts
@@ -1,35 +1,33 @@
-import {test,expect, Page} from "playwright/test";
+import { test, expect, Page } from "playwright/test";
 import { BrowserWrapper } from "../infra/browser-wrapper";
-import config from '../../config.json';
-import { Header } from "../logic/components/header";
+import config from "../../config.json";
 import { SearchPage } from "../logic/search-page";
+import { HomePage } from "../logic/home-page";
 
-test.describe('search about item verify results',()=>{
-    let page: Page;
-    let browser: BrowserWrapper;
-    let searchpage : SearchPage
-    test.beforeAll(async () => {
-    browser = new BrowserWrapper;
-    });
+test.describe("search about item verify results", () => {
+  let page: Page;
+  let browser: BrowserWrapper;
+  let searchpage: SearchPage;
 
-    test.beforeEach(async () => {
-        page = await browser.getPage(config.baseUrl)
-        const header = new Header(page)
-        header.clickOnSearchIcon()
-        await header.fillInSearchItem("adidas")
-        searchpage = new SearchPage(page)
+  test.beforeAll(async () => {
+    browser = new BrowserWrapper();
+  });
 
-    });
+  test.beforeEach(async () => {
+    page = await browser.getPage(config.baseUrl);
+    const homepage = new HomePage(page);
+    homepage.clicOnSearchHeaderBtn();
+    await homepage.fillSearchInputHeader("adidas");
+    searchpage = new SearchPage(page);
+  });
 
-    test("Search about a brand and validate items brand",async()=>{
-        expect(await searchpage.getRandomItemBrand()).toContain("ADIDAS") 
+  test("Search about a brand and validate items brand", async () => {
+    expect(await searchpage.getRandomItemBrand()).toContain("ADIDAS");
+  });
 
-    });
-
-    test("Search about a brand and validate the result title text",async()=>{
-        expect(await searchpage.isResultTitleContainBrandName("ADIDAS")).toBeTruthy() 
-    });
-
-
-
+  test("Search about a brand and validate the result title text", async () => {
+    expect(
+      await searchpage.isResultTitleContainBrandName("ADIDAS")
+    ).toBeTruthy();
+  });
 });

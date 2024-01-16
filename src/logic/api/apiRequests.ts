99d425662c56450f68199f9e89093bac16004702
Mohamed Omarii <mohamed.44301hotmail.com>
2024-01-16 16:53:43 +0200

    fix authentication for login via api.

--- src/logic/api/apiRequests.ts
+++ src/logic/api/apiRequests.ts
@@ -5,13 +5,14 @@ import { requestUrls } from "../../../apiUrls.json";
 import { Response } from "./resonseBody/responseFromApi";
 import { LoginCredentials } from "../api/requestBody/login-request";
 import { LoginResponse } from "./resonseBody/login-response";
+import { APIRequestContext } from "playwright";
 
 export class ApiCall{
     addItemToCart = async (data: CartRequest): Promise<Response<AddItemResponse>> => {
         return await apiPostMethod(requestUrls.addItemToCart, data);
     }
     
-    loginApi = async (data: LoginCredentials): Promise<Response<LoginResponse>> => {
-        return await apiPostMethod(requestUrls.loginViaApi, data);
+    loginApi = async (data: LoginCredentials, request: APIRequestContext): Promise<Response<LoginResponse>> => {
+        return await apiPostMethod(requestUrls.loginViaApi, data, request);
     };
 }
--- src/tests/auth.setup.ts
+++ src/tests/auth.setup.ts
@@ -3,14 +3,17 @@ import { test as setup } from '@playwright/test';
 import { ApiCall } from '../logic/api/apiRequests';
 import config from '../../config.json';
 import { BuildLoginRequest } from '../logic/api/requestBody/login-request';
+import { after } from 'node:test';
 
 const authFile = 'playwright/.auth/user.json';
 
-setup('authenticate', async ({ page }) => {
-    await page.goto(config.baseUrl);
+setup('authenticate', async ({ request }) => {
     const data = BuildLoginRequest(config.email, config.password);
     const apiCallls = new ApiCall()
-    const result = await apiCallls.loginApi(data)
-    console.log(result)
-    await page.context().storageState({ path: authFile });
+    const result = await apiCallls.loginApi(data, request)
+    await request.storageState({ path: authFile });
+});
+
+after(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
 });
